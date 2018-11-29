049592c393e8604e83f3942c3082867cd5c91a64
ceccs17d28 <43616819+ceccs17d28@users.noreply.github.com>
2018-11-29 16:43:39 +0530

    Update 26_Queue using Array.c

--- 26_Queue using Array.c
+++ 26_Queue using Array.c
@@ -1,85 +1,107 @@
 /* Queue using Array
    Jeny Susan Rajan S3 D
    Roll no: 28 */
-#include <stdio.h>
 
-int queue[100], n=100, front=-1, rear=-1;
 
-void enqueue() 
+#include<stdio.h>
+#include<stdlib.h>
+
+void main()
 {
-	int data;
-	if (rear == n-1) 
+    int queue[15],i,front=0,choice,end=0,ele,cho,limit=14;
+
+    do
     {
-		printf("Failed: Queue full\n");
-	}
-    else
-    {
-		printf("Enter data to be entered : ");
-		scanf("%d", &data);
-		if (front == -1) 
+        printf("\nMENU\n1.Insertion\n2.Deletion\n3.Display\n4.Exit\nEnter Choice: ");
+        scanf("%d",&choice);
+
+        switch(choice)
         {
-			front = rear = 0;
-		} 
-        else 
-        {
-			rear++;
-		}
-		queue[rear] = data;
-	}
+            case 1: printf("Enter the value of element: ");
+                    scanf("%d",&ele);
+                    if(front==0)
+                    {
+                        front=1;
+                        end=1;
+                        queue[end]=ele;
+                    }
+                    else if(end==limit)
+                    {
+                        printf("Queue is FULL");
+                    }
+                    else
+                    {
+                        end+=1;
+                        queue[end]=ele;
+                    }
+                    break;
+
+            case 2: if(front==0)
+                    {
+                        printf("Queue is EMPTY");
+                    }
+                    else if(front==end)
+                    {
+                        printf("Deleted %d",queue[end]);
+                        front=0;
+                        end=0;
+                    }
+                    else
+                    {
+                        printf("DeletedDD %d",queue[front]);
+                        front+=1; 
+                    }
+                    break;
+            case 3: if(front==0)
+                    {
+                        printf("Queue is EMPTY");
+                    }
+                    else
+                    {
+                        for(i=front;i<=end;i++)
+                        {
+                            printf("%d -> ",queue[i]);
+                        }
+                      
+                    }
+        } 
+    }while(choice==1 || choice==2 || choice==3);
+
 }
-void dequeue() 
-{
-	if (front == -1) 
-    {
-		printf("Failed: Empty queue\n");
-	} 
-    else 
-    {
-		printf("Deleted element : %d\n", queue[front]);
-		if (front == rear)
-        {
-			front = rear = -1;
-		} 
-        else
-        {
-			front++;
-		}
-	}
-}
-void printQueue() 
-{
-	int i=0;
-	if (front == -1) 
-    {
-		printf("Empty Queue\n");
-	} 
-    else 
-    {
-		for (i=front; i<=rear; i++)
-        {
-			printf("%d\n", queue[i]);
-		}
-	}
-}
-int main() 
-{
-	char choice;
-	front = rear = -1;
-	do 
-    {
-		printf("\n\tQueue using array\n1.Enqueu\n2.Dequeue\n3.Display\n4.Exit\nEnter your choice : ");
-		scanf(" %c", &choice);
-		switch(choice)
-        {
-			case '1':
-				enqueue();
-				break;
-			case '2':
-				dequeue();
-				break;
-			case '3':
-				printQueue();
-				break;
-		}
-	} while(choice != '4');
-}
+OUTPUT:
+MENU
+1.Insertion
+2.Deletion
+3.Display
+4.Exit
+Enter Choice:1 Enter the value of element:33
+MENU
+1.Insertion
+2.Deletion
+3.Display
+4.Exit
+Enter Choice:1 Enter the value of element: 12
+MENU
+1.Insertion
+2.Deletion
+3.Display
+4.Exit
+Enter Choice:1 Enter the value of element:87 
+MENU
+1.Insertion
+2.Deletion
+3.Display
+4.Exit
+Enter Choice: 33 -> 12 -> 87 -> 
+MENU
+1.Insertion
+2.Deletion
+3.Display
+4.Exit
+Enter Choice: DeletedDD 33
+MENU
+1.Insertion
+2.Deletion
+3.Display
+4.Exit
+Enter Choice: 4
