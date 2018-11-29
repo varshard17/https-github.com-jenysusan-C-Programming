d1cd9638cf6fbee9a81d62842323c9b6dc5954ca
ceccs17d28 <43616819+ceccs17d28@users.noreply.github.com>
2018-11-29 16:55:54 +0530

    Update 32_BinaryTree using Linked list.C

--- 32_BinaryTree using Linked list.C
+++ 32_BinaryTree using Linked list.C
@@ -149,3 +149,142 @@ void delete()
   } 
 
 }
+
+OUTPUT:
+
+
+          1. Insert in Binary Tree
+                   2. Delete from Binary Tree
+                   3. Inorder traversal of Binary tree
+                   4. Postorder traversal of Binary tree
+                   5. Preorder traversal of Binary tree
+                   6. Search and replace
+                   7. Exit
+                   Enter choice : 1
+
+           Enter new element: 4
+
+           root is 4
+           Inorder traversal of binary tree is :  4
+                   1. Insert in Binary Tree
+                   2. Delete from Binary Tree
+                   3. Inorder traversal of Binary tree
+                   4. Postorder traversal of Binary tree
+                   5. Preorder traversal of Binary tree
+                   6. Search and replace
+                   7. Exit
+                   Enter choice : 1
+
+           Enter new element: 4
+
+           root is 4
+           Inorder traversal of binary tree is :  4
+                   1. Insert in Binary Tree
+                   2. Delete from Binary Tree
+                   3. Inorder traversal of Binary tree
+                   4. Postorder traversal of Binary tree
+                   5. Preorder traversal of Binary tree
+                   6. Search and replace
+                   7. Exit
+                   Enter choice : 1
+
+           Enter new element: 6
+
+           root is 4
+           Inorder traversal of binary tree is :  4 6
+                   1. Insert in Binary Tree
+                   2. Delete from Binary Tree
+                   3. Inorder traversal of Binary tree
+                   4. Postorder traversal of Binary tree
+                   5. Preorder traversal of Binary tree
+                   6. Search and replace
+                   7. Exit
+                   Enter choice : 1
+
+           Enter new element: 5
+
+           root is 4
+           Inorder traversal of binary tree is :  4 5 6
+                   1. Insert in Binary Tree
+                   2. Delete from Binary Tree
+                   3. Inorder traversal of Binary tree
+                   4. Postorder traversal of Binary tree
+                   5. Preorder traversal of Binary tree
+                   6. Search and replace
+                   7. Exit
+                   Enter choice : 1
+
+           Enter new element: 8
+
+           root is 4
+           Inorder traversal of binary tree is :  4 5 6 8
+                   1. Insert in Binary Tree
+                   2. Delete from Binary Tree
+                   3. Inorder traversal of Binary tree
+                   4. Postorder traversal of Binary tree
+                   5. Preorder traversal of Binary tree
+                   6. Search and replace
+                   7. Exit
+                   Enter choice : 1
+
+           Enter new element: 7
+
+           root is 4
+           Inorder traversal of binary tree is :  4 5 6 7 8
+                   1. Insert in Binary Tree
+                   2. Delete from Binary Tree
+                   3. Inorder traversal of Binary tree
+                   4. Postorder traversal of Binary tree
+                   5. Preorder traversal of Binary tree
+                   6. Search and replace
+                   7. Exit
+                   Enter choice : 2
+
+           Enter the element to be deleted : 5
+           4 6 7 8
+                   1. Insert in Binary Tree
+                   2. Delete from Binary Tree
+                   3. Inorder traversal of Binary tree
+                   4. Postorder traversal of Binary tree
+                   5. Preorder traversal of Binary tree
+                   6. Search and replace
+                   7. Exit
+                   Enter choice : 4
+
+           Postorder traversal of binary tree is :  7 8 6 4
+                   1. Insert in Binary Tree
+                   2. Delete from Binary Tree
+                   3. Inorder traversal of Binary tree
+                   4. Postorder traversal of Binary tree
+                   5. Preorder traversal of Binary tree
+                   6. Search and replace
+                   7. Exit
+                   Enter choice : 5
+
+           Preorder traversal of binary tree is :  4 6 8 7
+                   1. Insert in Binary Tree
+                   2. Delete from Binary Tree
+                   3. Inorder traversal of Binary tree
+                   4. Postorder traversal of Binary tree
+                   5. Preorder traversal of Binary tree
+                   6. Search and replace
+                   7. Exit
+                   Enter choice : 6
+
+           Search and replace operation in binary tree
+           Enter the element to be searched :8
+
+           Element 8 which was searched is found and is = 8
+           Do you want replace it, press 1 for yes :
+          1
+
+           Enter new element :7
+
+                   1. Insert in Binary Tree
+                   2. Delete from Binary Tree
+                   3. Inorder traversal of Binary tree
+                   4. Postorder traversal of Binary tree
+                   5. Preorder traversal of Binary tree
+                   6. Search and replace
+                   7. Exit
+                   Enter choice : 7
