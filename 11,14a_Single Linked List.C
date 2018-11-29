46adc0606b9363170fa630d7a734318768d8abe5
ceccs17d28 <43616819+ceccs17d28@users.noreply.github.com>
2018-11-29 16:17:16 +0530

    Update 11,14a_Single Linked List.C

--- 11,14a_Single Linked List.C
+++ 11,14a_Single Linked List.C
@@ -4,147 +4,219 @@
 
 #include<stdio.h>
 #include<stdlib.h>
-struct node
-{
-	int data;
-    	struct node *link;
-};
-void main()
-{
-    	int key,x,c,l,n;
-    	struct node *head,*temp,*ptr,*ptr1;
-    	head=(struct node*)malloc(sizeof(struct node));
-    	head->data=0;
-    	head->link=NULL;
 
-    	do
-    	{
-        	ptr=head;
-        	printf("\nMENU\n1.insertion(begin)\n2.Insertion(end)\n3.Insertion(after key)\n4.Deletion(front)\n5.Deletion(end)\n6.Deletion(key)\n7.Search\n8.Display\nEnter choice:");
-        	scanf("%d",&c);
-		    if(c==1)
-		    {
-			    ptr=head;
-			    temp=(struct node*)malloc(sizeof(struct node));
-			    temp->link=ptr->link;	
-			    ptr->link=temp;
-			    printf("Enter data: ");
-            	scanf("%d",&x);
-            	temp->data=x;
-		    }
-        	else if(c==2)
-        	{
-            	while(ptr->link!=NULL)
-            		{
-                	ptr=ptr->link;
-            		}
-            	temp=(struct node*)malloc(sizeof(struct node));
-            	ptr->link=temp;
-        		printf("Enter data: ");
-        		scanf("%d",&x);
-            	temp->data=x;
-            	temp->link=NULL;
-        	}
-        	else if(c==3)
-        	{
-            	printf("Enter key: ");
-            	scanf("%d",&key);
-        		ptr=ptr->link;
-        		while(ptr!=NULL)
-        		{
-                	if(ptr->data==key)
-                		{
-                    			temp=(struct node*)malloc(sizeof(struct node));
-                    			ptr1=ptr->link;
-                    			ptr->link=temp;
-                    			temp->link=ptr1;
-                    			printf("Enter data: ");
-                    			scanf("%d",&x);
-                    			temp->data=x;
-				}
-               			ptr=ptr->link;
-            		}
-        	}
-		else if(c==4)
-		{
-			ptr=head;
-			ptr1=ptr->link;
-			ptr->link=ptr1->link;
-			free(ptr1);
-		}
-		else if(c==5)
-		{
-			while(ptr->link!=NULL)
-            		{
-				        ptr1=ptr;
-                		ptr=ptr->link;
-            		}
-			ptr1->link=NULL;
-		}
-        else if(c==6)
-        	{
-            		if(ptr->link==NULL)
-            		{
-                		printf("List is empty!!!");
-            		}
-            		else
-            		{
-                		printf("Enter key: ");
-                		scanf("%d",&key);
-                		l=0;
-                		while(ptr->link!=NULL)
-                		{
-                    			ptr1=ptr;
-                    			ptr=ptr->link;
-                    			if(ptr->data==key)
-                    			{
-                        			ptr1->link=ptr->link;
-                        			free(ptr);
-                        			l+=1;
-                    			}
-                		}
-                		if(l==0)
-                		{
-                    			printf("Key not found!!!");
-                		}
-            		}
-        	}
-        	else if(c==7)
-        	{
-            		if(ptr->link==NULL)
-            		{
-                		printf("Empty list!!!");
-            		}
-            		ptr=ptr->link;
-            		l=0;
-			n=0;
-            		printf("Enter key: ");
-            		scanf("%d",&key);
-            		while(ptr!=NULL)
-            		{
-				n=n+1;
-                		if(ptr->data==key)
-                		{
-                    			printf("\nElement found at %d",n);
-                    			l+=1;
-					break;
-                		}
-                		ptr=ptr->link;
-            		}
-            		if(l==0)
-            		{
-                		printf("\nElement not found...");
-            		}
-        	}
-        	else if(c==8)
-        	{
-            		ptr=ptr->link;
-            		while(ptr!=NULL)
-            		{
-                		printf("%d -> ",ptr->data);
-                		ptr=ptr->link;
-            		}
-            		printf("NULL");
-        	}
-    	}while(c==1 || c==2 || c==3 || c==4 || c==5 || c==6 || c==7 || c==8);
+struct Node {
+	int data;
+	struct Node *next;
+}*Header;
+int DeleteAll()
+{
+	struct Node *ptr=Header, *prevprt;
+	while(ptr!=NULL)
+	{
+		prevprt=ptr;
+		ptr=ptr->next;
+		free(prevprt);
+	}
 }
+struct Node *newNode()
+{
+	struct Node *newptr = malloc(sizeof(struct Node));
+	if (newptr == NULL)
+	{
+		printf("Memory Overflow");
+		DeleteAll();
+		exit(0);
+	}
+	return (newptr);
+};
+int DeleteNode(int data)
+{
+	struct Node *currentNode= Header,*prevNode;
+	while(currentNode->data != data && currentNode->next!= NULL)
+	{
+		prevNode= currentNode;
+		currentNode = currentNode->next;
+	}	
+	if (currentNode->data !=data)
+		printf("Node not found.\n Deletion Failed \n" );
+	else
+		{
+			prevNode->next=currentNode->next;
+			free(currentNode);
+		}
+}
+int insert(int data)
+{
+	struct Node *ptr = newNode();
+	ptr->next =Header->next;
+	ptr->data = data;
+	Header->next= ptr;
+}
+int insertAfter(int prevdata, int data)
+{
+	struct Node *currentNode = Header, *ptr= newNode();
+	while(currentNode->next != NULL && currentNode->data != prevdata)
+		currentNode= currentNode->next;
+	if(currentNode->data != prevdata)
+		printf("Node not found\nInsertion Failed \n");
+	else 
+	{
+		ptr->next = currentNode->next;
+		ptr->data = data;
+		currentNode->next = ptr;
+	}
+	return (0);
+
+}
+int append(int data)
+{
+	struct Node *newptr = newNode(), *currentNode = Header;
+
+	while (currentNode->next != NULL) {
+		currentNode = currentNode->next;
+	}
+
+	newptr->next = currentNode->next;
+	newptr->data = data;
+	currentNode->next = newptr;
+	return (0);
+}
+int printlist() {
+	struct Node *currentNode = Header->next;
+	printf("\nLinked List\nHeader->");
+	while (currentNode != NULL) {
+		printf("%d->", currentNode->data);
+		currentNode = currentNode->next;
+	}
+	return (0);
+}
+int ShowMenu()
+{
+	int i=1,data,prevdata;
+	while(i!=0)
+	{	
+		printf("\n\tLINKED LIST\n");
+		printf("1. Insert at Front\n");
+		printf("2. Insert after\n");
+		printf("3. Insert at End\n");
+		printf("4. Delete node\n");
+		printf("5. Delete all\n");
+		printf("0. Exit\n");
+		printf("Enter your choice : ");
+		scanf("%d",&i);
+		
+		switch(i)
+		{
+			case 1:
+			{	
+				printf("Enter the data to insert:");
+				scanf("%d",&data);
+				insert(data);
+				printlist();
+			
+				break;
+	
+			}
+			case 2:
+			{	
+				printf("Enter the data after which node is to be created and the data to be inserted:");
+				scanf("%d %d",&prevdata,&data);
+				insertAfter(prevdata,data);
+				printlist();
+				break;
+			}
+			case 3:
+			{
+		
+				printf("Enter the data to insert:");
+				scanf("%d",&data);
+				append(data);
+				printlist();
+				break;
+			}
+			case 4:
+			{
+				printf("Enter data to be deleted : ");
+				scanf("%d", &data);
+				DeleteNode(data);
+				printlist();
+				break;	
+			}
+			case 5:
+			{
+				DeleteAll();
+				break;
+			}
+
+			case 0:
+			{	
+				i=0;
+				break;
+			}
+		}
+	}	
+	return 0;
+}
+
+int main()
+{
+	Header = malloc(sizeof(struct Node));
+	Header->data = 0;
+	Header->next = NULL;
+	ShowMenu();
+	return (0);
+}
+
+OUTPUT:
+
+	LINKED LIST
+1. Insert at Front
+2. Insert after
+3. Insert at End
+4. Delete node
+5. Delete all
+0. Exit
+Enter your choice :1 Enter the data to insert:3
+Linked List
+Header->3->
+	LINKED LIST
+1. Insert at Front
+2. Insert after
+3. Insert at End
+4. Delete node
+5. Delete all
+0. Exit
+Enter your choice :2 Enter the data after which node is to be created and the data to be inserted:3 Enter the new data: 4
+
+Linked List
+Header->3->4->
+	LINKED LIST
+1. Insert at Front
+2. Insert after
+3. Insert at End
+4. Delete node
+5. Delete all
+0. Exit
+Enter your choice :3 Enter the data to insert:5
+Linked List
+Header->3->4->5->
+	LINKED LIST
+1. Insert at Front
+2. Insert after
+3. Insert at End
+4. Delete node
+5. Delete all
+0. Exit
+Enter your choice : 4 Enter data to be deleted :4 
+Linked List
+Header->3->5->
+
+	LINKED LIST
+1. Insert at Front
+2. Insert after
+3. Insert at End
+4. Delete node
+5. Delete all
+0. Exit
+Enter your choice : 0
