c2b2272b812d87eee153d4fbdb762ca845dabbd0
ceccs17d28 <43616819+ceccs17d28@users.noreply.github.com>
2018-11-29 16:02:45 +0530

    Update 08_QuickSort(Recursion).C

--- 08_QuickSort(Recursion).C
+++ 08_QuickSort(Recursion).C
@@ -3,48 +3,51 @@
    Roll no: 28 */
 
 #include<stdio.h>
-void Quicksort(int[],int,int);
+void quicksort(int num[25],int first,int last)
+{
+	int i,j,pivot,temp;
+	if(first<last)
+	{
+		pivot = first;
+		i = first; 
+		j = last;
+		while(i<j){
+			while(num[i]<=num[pivot] &&i<last)
+				i++;
+			while(num[j]>num[pivot])
+				j--;
+			if(i<j)
+			{
+				temp = num[i];
+				num[i] = num[j];
+				num[j] = temp;
+			}
+		}	
+		
+	
+		temp = num[pivot];
+		num[pivot] = num[j];
+		num[j] = temp;
+		quicksort(num,first,j-1);
+		quicksort(num,j+1,last);
+	}
+}
 int main()
 {
-  int i,count,no[25];
-  printf("Enter size:");
-  scanf("%d",&count);
-  printf("Enter %d elements:",count);
-  for(i=0;i<count;i++)
-  	scanf("%d",&no[i]);
-  Quicksort(no,0,count-1);
-  printf("The sorted elements are:");
-  for(i=0;i<count;i++)
-  	printf("%d",no[i]);
-  return 0;
+	int i,count,num[25];
+	printf("How many elements?");
+	scanf("%d",&count);
+	printf("Enter %d elements:",count);
+	for(i=0;i<count;i++)
+		scanf("%d",&num[i]);
+	quicksort(num,0,count-1);
+	printf("ORDERED:");
+	for(i=0;i<count;i++)
+		printf("%d ",num[i]);
+	return 0;
 }
-
-void Quicksort(int no[25],int first,int last)
-{
-  int i,j,pivot,temp;
-  if(first<last)
-  { 
-    pivot=first;
-    i=first;
-    j=last;
-    while(i<j)
-    {
-	while((no[i]<=no[pivot]) &&(i<last))
-		i++;
-	while (no[j]>no[pivot])
-		j--;
-	if(i<j)
-	{
-		temp=no[i];
-		no[i]=no[j];
-		no[j]=temp;
-	}
-    }
-    temp=no[pivot];
-    no[pivot]=no[j];
-    no[j]=temp;
-    Quicksort(no,first,j-1);
-    Quicksort(no,j+1,last);
-  }
-}
+OUTPUT:
+How many elements?5
+Enter 5 elements:4 7 2 9 1
+ORDERED:1 2 4 7 9 
   	
