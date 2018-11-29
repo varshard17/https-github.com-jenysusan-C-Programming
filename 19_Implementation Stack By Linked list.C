8231a6b9030836a025e47a519280a295e6e07629
ceccs17d28 <43616819+ceccs17d28@users.noreply.github.com>
2018-11-29 16:30:44 +0530

    Update 19_Implementation Stack By Linked list.C

--- 19_Implementation Stack By Linked list.C
+++ 19_Implementation Stack By Linked list.C
@@ -4,80 +4,170 @@
 
 
 
-#include<stdio.h>
-#include<conio.h>
+#include <stdio.h>
+#include <stdlib.h>
 
-struct Node
-{
-   int data;
-   struct Node *next;
-}*top = NULL;
+struct node {
+	int data;
+	struct node *next;
+}*TOP;
 
-void push(int);
-void pop();
-void display();
-
-void main()
-{
-   int choice, value;
-   clrscr();
-   printf("\n:: Stack using Linked List ::\n");
-   while(1){
-      printf("\n****** MENU ******\n");
-      printf("1. Push\n2. Pop\n3. Display\n4. Exit\n");
-      printf("Enter your choice: ");
-      scanf("%d",&choice);
-      switch(choice){
-	 case 1:      printf("Enter the value to be insert: ");
-		           scanf("%d", &value);
-		           push(value);
-		           break;
-	 case 2:      pop(); break;
-	 case 3:      display(); break;
-	 case 4:      exit(0);
-	 default: printf("\nWrong selection!!! Please try again!!!\n");
-      }
-   }
+int deleteAll() {
+	struct node *ptr = TOP, *prevPtr;
+	while (ptr != NULL) {
+		prevPtr = ptr;
+		ptr = ptr->next;
+		free(prevPtr);
+	}
+	TOP->next = NULL;
+	return (0);
 }
-
-void push(int value)
-{
-   struct Node *newNode;
-   newNode = (struct Node*)malloc(sizeof(struct Node));
-   newNode->data = value;
-   if(top == NULL)
-      newNode->next = NULL;
-   else
-      newNode->next = top;
-   top = newNode;
-   printf("\nInsertion is Success!!!\n");
+struct node *newNode() {
+	struct node *newptr = malloc(sizeof(struct node));
+	if (newptr == NULL) {
+		printf("Memory overflow");
+		deleteAll();
+		exit(0);
+	}
+	return (newptr);
 }
-
-void pop()
-{
-   if(top == NULL)
-      printf("\nStack is Empty!!!\n");
-   else
-   {
-      struct Node *temp = top;
-      printf("\nDeleted element: %d", temp->data);
-      top = temp->next;
-      free(temp);
-   }
+int deleteFirstNode() {
+	struct node *currentNode = TOP->next;
+	if (currentNode == NULL) {
+		printf("Empty stack.\nDeletion failed.\n");
+	} else {
+		TOP->next = currentNode->next;
+		free(currentNode);
+	}
+	return (0);
 }
+int insertNodeFront(int data) {
+	struct node *newptr = newNode();
+	newptr->next = TOP->next;
+	newptr->data = data;
+	TOP->next = newptr;
+	return (0);
+}
+int printLinkedList() {
+	struct node *currentNode = TOP->next;
+	printf("\nLinked List\nTOP->");
+	while (currentNode != NULL) {
+		printf("%d\n", currentNode->data);
+		currentNode = currentNode->next;
+	}
+	printf("\n");
+	return (0);
+}
+int showMenu() {
+	char choice='1';
+	int data, previousData, nextData;
+	do {
+		printf("\n\tLINKED LIST\n");
+		printf("1. Push\n");
+		printf("2. Pop\n");
+		printf("3. Print\n");
+		printf("4. Exit\n");
+		printf("Enter your choice : ");
+		scanf(" %c", &choice);
+		printf("\n");
+		switch (choice) {
+			case '1':
+				printf("Enter new data : ");
+				scanf("%d", &data);
+				insertNodeFront(data);
+				break;
+			case '2':
+				deleteFirstNode();
+				break;
+			case '3':
+				printLinkedList();
+				break;
+			case '4':
+				deleteAll();
+				exit(0);
+				break;
+			default:
+                printf("Invalid choice\n");
+				break;
+		}
+	} while(choice != '4');
+	return (0);
+}
+int main() {
+	// initialize Linked List
+	TOP = malloc(sizeof(struct node));
+	TOP->data = 0;
+	TOP->next = NULL;
+	// finished initializing Linked List
 
-void display()
-{
-   if(top == NULL)
-      printf("\nStack is Empty!!!\n");
-   else
-   {
-      struct Node *temp = top;
-      while(temp->next != NULL){
-	   printf("%d--->",temp->data);
-	   temp = temp -> next;
-   }
-   printf("%d--->NULL",temp->data);
-   
-}    
+	showMenu();
+	deleteAll();
+	
+	return (0);
+}
+OUTPUT:
+
+	LINKED LIST
+1. Push
+2. Pop
+3. Print
+4. Exit
+Enter your choice :1 
+Enter new data : 8
+	LINKED LIST
+1. Push
+2. Pop
+3. Print
+4. Exit
+Enter your choice :1 
+Enter new data : 9
+	LINKED LIST
+1. Push
+2. Pop
+3. Print
+4. Exit
+Enter your choice :1 
+Enter new data : 2
+	LINKED LIST
+1. Push
+2. Pop
+3. Print
+4. Exit
+Enter your choice :3 
+
+Linked List
+TOP->2
+9
+8
+
+
+	LINKED LIST
+1. Push
+2. Pop
+3. Print
+4. Exit
+Enter your choice : 2
+
+	LINKED LIST
+1. Push
+2. Pop
+3. Print
+4. Exit
+Enter your choice : 2
+
+	LINKED LIST
+1. Push
+2. Pop
+3. Print
+4. Exit
+Enter your choice : 2
+
+	LINKED LIST
+1. Push
+2. Pop
+3. Print
+4. Exit
+Enter your choice : 2
+Empty stack.
+Deletion failed.
     
